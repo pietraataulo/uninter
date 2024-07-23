55ca61a03bfb9f8cb1f370709c3cafa12c34a623
Pietra Ataulo <pietraataulo@gmail.com>
2024-07-23 16:01:00 -0300

    .

---
+++ disciplinas/programacao-estruturada/aula0/base.c
@@ -0,0 +1,10 @@
+// Estrutura base
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    return 0;
+}
---
+++ disciplinas/programacao-estruturada/aula0/ex001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    int x, y, res;
+    printf("Digite um valor:\n");
+    scanf("%i", &x);
+
+
+    return 0;
+}
